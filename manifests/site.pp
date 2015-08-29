34d423154a471d3e947f26c47935ab83a38189fa
Pongstr <twiz.tickler@gmail.com>
2015-08-29 18:45:21 +0800

    Hammer of the Gods

--- manifests/site.pp
+++ manifests/site.pp
@@ -116,28 +116,116 @@ node default {
   # -------
   # Creates global default Log and Data locations
   # and sets default host and port at the same time
-  file { '/opt/boxen/data/mongodb':
-    ensure => directory,
+  # file { "/opt/boxen/data/mongodb":
+  #   ensure => directory,
+  # }
+
+  # exec { "Create MongoDB Data Path":
+  #   command => "mkdir -p /opt/boxen/data/mongodb/data",
+  #   creates => "/opt/boxen/data/mongodb/data",
+  #   onlyif  => ["test ! -d /opt/boxen/data/mongodb/data"],
+  # }
+
+  # exec { 'Create MongoDB Log Path':
+  #   command => "mkdir -p /opt/boxen/data/mongodb/logs",
+  #   creates => "/opt/boxen/data/mongodb/logs",
+  #   onlyif  => ["test ! -d /opt/boxen/data/mongodb/logs"],
+  # }
+
+  # class { 'mongodb':
+  #   host    => '127.0.0.1',
+  #   port    => '27017',
+  #   logdir  => "/opt/boxen/data/mongodb/logs",
+  #   datadir => "/opt/boxen/data/mongodb/data",
+  # }
+
+  # NPM Modules
+  # -----------
+  # Default Mjolnir modules for development
+  # and convenience.
+  $node_version = '0.12.7'
+
+  npm_module { 'Bower Browser Package Manager':
+    module       => 'bower',
+    version      => '^1.5.0',
+    node_version => '*',
   }
 
-  exec { 'Create MongoDB Data Path':
-    command => "mkdir -p /opt/boxen/data/mongodb/data",
-    creates => "/opt/boxen/data/mongodb/data",
-    onlyif  => ["test ! -d /opt/boxen/data/mongodb"],
+  npm_module { 'CoffeeScript':
+    module       => 'coffee-script',
+    version      => '^1.9.0',
+    node_version => $node_version,
   }
 
-  exec { 'Create MongoDB Log Path':
-    command => "mkdir -p /opt/boxen/data/mongodb/logs",
-    creates => "/opt/boxen/data/mongodb/logs",
-    onlyif  => ["test ! -d /opt/boxen/data/mongodb"],
+  npm_module { 'ExpressJS':
+    module       => 'express',
+    version      => '^4.13.0',
+    node_version => $node_version,
   }
 
-  class { 'mongodb':
-    host    => '127.0.0.1',
-    port    => '27017',
-    logdir  => "/opt/boxen/data/mongodb/logs",
-    datadir => "/opt/boxen/data/mongodb/data",
+  npm_module { 'Grunt CLI':
+    module       => 'grunt-cli',
+    version      => '^4.13.0',
+    node_version => $node_version,
   }
 
-  # Node Packages
+  npm_module { 'Gulp Build System':
+    module       => 'gulp',
+    version      => '^3.9.0',
+    node_version => $node_version,
+  }
+
+  npm_module { 'JSHint':
+    module       => 'jshint',
+    version      => '^2.8.0',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Karma CLI':
+    module       => 'karma-cli',
+    version      => '^0.1.0',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Less Leaner CSS':
+    module       => 'less',
+    version      => '^2.5.0',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Strongloop CLI':
+    module       => 'strongloop',
+    version      => '^5.0.0',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Mongoose':
+    module       => 'mongoose',
+    version      => '^4.1.3',
+    node_version => $node_version,
+  }
+
+  npm_module { 'NodeMon':
+    module       => 'nodemon',
+    version      => '^1.4.1',
+    node_version => $node_version,
+  }
+
+  npm_module { 'PM2':
+    module       => 'pm2',
+    version      => '^0.14.7',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Sails.js':
+    module       => 'sails',
+    version      => '^0.11.0',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Shelljs':
+    module       => 'shelljs',
+    version      => '^0.5.3',
+    node_version => $node_version,
+  }
 }
