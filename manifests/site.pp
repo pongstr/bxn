f2846efb78337b41345048658a05e65b3de556e8
Pongstr <twiz.tickler@gmail.com>
2015-08-30 09:16:49 +0800

    Hammer of the Gods
    We come from the land of the ice and snow in the midnight sun
    where the hot springs flow, The hammer of the gods will drive our
    ships to new lands to fight the horde, singing and crying:
    
    Valhalla I'am coming...
    
    We come from the land of the ice and snow in the midnight sun
    where the hot springs flow, How soft your fields of green,
    can whisper tales of gore of how we calmed the tides of war:
    
    We are your overlords..
    
    On we sweep, with threshing oar, Our only goal will be the
    western shore

--- manifests/site.pp
+++ manifests/site.pp
@@ -55,15 +55,19 @@ node default {
   include atom
   include brewcask
   include chrome
+  include chrome::canary
   include cyberduck
   include dashlane
   include dnsmasq
   include firefox
+  include firefox::aurora
   include git
   include hipchat
   include hub
+  include mongodb
   include nginx
   include python
+  include redis
   include sublime_text
   include virtualbox
   include vlc
@@ -82,6 +86,7 @@ node default {
   # default ruby versions
   ruby::version { '1.9.3': }
   ruby::version { '2.1.3': }
+  ruby::version { '2.1.6': }
 
   # Taps for Homebrew
   homebrew::tap { 'caskroom/versions': }
@@ -112,32 +117,140 @@ node default {
   # Team Mjolnir Customizations
   # #########################################################################
 
+  $home_directory = "/Users/${::boxen_user}"
+  $node_version   = '0.12.7'
+  $ruby_version   = '2.1.6'
+
   # MongoDB
   # -------
   # Creates global default Log and Data locations
   # and sets default host and port at the same time
-  file { '/opt/boxen/data/mongodb':
+  # file { "/opt/boxen/data/mongodb":
+  #   ensure => directory,
+  # }
+
+  # NPM Modules
+  # -----------
+  # Default Mjolnir modules for development and convenience.
+  npm_module { 'Bower Browser Package Manager':
+    module       => 'bower',
+    node_version => '*',
+  }
+
+  npm_module { 'CoffeeScript':
+    module       => 'coffee-script',
+    node_version => $node_version,
+  }
+
+  npm_module { 'ExpressJS':
+    module       => 'express',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Grunt CLI':
+    module       => 'grunt-cli',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Gulp Build System':
+    module       => 'gulp',
+    node_version => $node_version,
+  }
+
+  npm_module { 'JSHint':
+    module       => 'jshint',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Karma CLI':
+    module       => 'karma-cli',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Less Leaner CSS':
+    module       => 'less',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Strongloop CLI':
+    module       => 'strongloop',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Mongoose':
+    module       => 'mongoose',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Nodemon':
+    module       => 'nodemon',
+    node_version => $node_version,
+  }
+
+  npm_module { 'PM2':
+    module       => 'pm2',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Sails.js':
+    module       => 'sails',
+    node_version => $node_version,
+  }
+
+  npm_module { 'Shelljs':
+    module       => 'shelljs',
+    node_version => $node_version,
+  }
+
+  # Ruby Gems
+  # ---------
+  # Default Mjolnir gems for development and convenience.
+  ruby_gem { 'bundler for all rubies':
+    gem          => 'bundler',
+    version      => '~> 1.10.6',
+    ruby_version => '*',
+  }
+
+  ruby_gem { 'bootstrap sass':
+    gem          => 'bootstrap-sass',
+    version      => '~> 3.3.5',
+    ruby_version => $ruby_version,
+  }
+
+  ruby_gem { 'sass compass':
+    gem          => 'compass',
+    version      => '~> 1.0.3',
+    ruby_version => $ruby_version,
+  }
+
+  ruby_gem { 'zurb foundation':
+    gem          => 'foundation',
+    version      => '~> 1.0.4',
+    ruby_version => $ruby_version,
+  }
+
+  ruby_gem { 'github-pages':
+    gem          => 'github-pages',
+    version => '~> 39',
+    ruby_version => $ruby_version,
+  }
+
+
+  # RoboMongo
+  package { 'robomongo':
+    provider => 'brewcask',
+    ensure   => installed,
+  }
+
+  # Oh-My-ZSH
+  file { "${home_directory}/.oh-my-zsh":
     ensure => directory,
   }
 
-  exec { 'Create MongoDB Data Path':
-    command => "mkdir -p /opt/boxen/data/mongodb/data",
-    creates => "/opt/boxen/data/mongodb/data",
-    onlyif  => ["test ! -d /opt/boxen/data/mongodb"],
+  exec { 'install oh-my-zsh plugin':
+    command => "curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh",
+    onlyif => [
+      "test ! -d ${home_directory}/.oh-my-zsh"
+    ]
   }
-
-  exec { 'Create MongoDB Log Path':
-    command => "mkdir -p /opt/boxen/data/mongodb/logs",
-    creates => "/opt/boxen/data/mongodb/logs",
-    onlyif  => ["test ! -d /opt/boxen/data/mongodb"],
-  }
-
-  class { 'mongodb':
-    host    => '127.0.0.1',
-    port    => '27017',
-    logdir  => "/opt/boxen/data/mongodb/logs",
-    datadir => "/opt/boxen/data/mongodb/data",
-  }
-
-  # Node Packages
 }
