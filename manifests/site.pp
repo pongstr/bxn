cf663457e084095931e6c3828b91469a50c04773
Pongstr <twiz.tickler@gmail.com>
2015-09-09 23:25:20 +0800

    updates for nodejs v4.0.0

--- manifests/site.pp
+++ manifests/site.pp
@@ -82,8 +82,8 @@ node default {
 
   # node versions
   nodejs::version { '0.12': }
-  nodejs::version { '0.12.5': }
   nodejs::version { '0.12.7': }
+  nodejs::version { '4.0.0': }
 
   # default ruby versions
   ruby::version { '1.9.3': }
@@ -123,7 +123,7 @@ node default {
   $custom_dotfiles = "${home_directory}/.dotfiles"
   $custom_projects = "${home_directory}/Projects"
 
-  $node_version = '0.12.7'
+  $node_version = '4.0.0'
   $ruby_version = '2.1.6'
 
   class { 'nodejs::global': version => $node_version }
