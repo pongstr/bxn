acfd4d43fc61d7ccfc918ee5fd9bf26b150cf54a
Pongstr <twiz.tickler@gmail.com>
2015-10-06 18:03:31 +0800

    Added Env Vars for Darkstar
    - added environment variables for Darkstar Project
    - ensure specific commit for pongstr/dotfiles

--- modules/projects/manifests/darkstar.pp
+++ modules/projects/manifests/darkstar.pp
@@ -1,17 +1,41 @@
 class projects::darkstar {
 
-  $darkstar = "uikit"
-  $boxen = "/opt/boxen/repo/modules/projects/templates"
-  $home  = "/Users/${::boxen_user}/Projects"
-  $path  = "${home}/${darkstar}"
+  # Project Properties
+  $uikit_project = "uikit"
+  $uikit_conf = "${boxen::config::home}"
+  $uikit_file = "${uikit_conf}/repo/modules/projects/templates"
+  $uikit_path = "/Users/${::boxen_user}/Projects/${uikit_project}"
 
-  file { $path: ensure => directory }
+  # Dev Environment Variables
+  $uikit_env = "
+# UI-Kit Dev Env Variables
+export BOXEN_UIKIT_APP_URL=coder.dev
+export BOXEN_UIKIT_APP_NAME=coder
+export BOXEN_UIKIT_APP_REPO=git@bitbutcket.org:torodev/darkstar.io.git
+export BOXEN_UIKIT_APP_PORTS=\"5000 5001 5002 5003 5004\"
+export BOXEN_UIKIT_APP_VERSION=2.0.0
+export BOXEN_UIKIT_APP_REPO_REF=develop
+export BOXEN_UIKIT_MAIL_HOST=smtp.gmail.com
+export BOXEN_UIKIT_MAIL_PORT=465
+export BOXEN_UIKIT_MAIL_USER=toro.repo@toro.io
+export BOXEN_UIKIT_MAIL_PASS=REDACTED
+export BOXEN_UIKIT_CDN=rackspace
+export BOXEN_UIKIT_CDN_USER=REDACTED
+export BOXEN_UIKIT_CDN_APIKEY=REDACTED
+export BOXEN_UIKIT_CDN_REGION=ORD
+  "
 
-  boxen::project { $darkstar:
-    dir     => $path,
+  file { $uikit_path: ensure => 'directory' }
+
+  file { "${uikit_conf}/env.d/110_${uikit_project}.sh":
+    content => "$uikit_env",
+  }
+
+  boxen::project { $uikit_project:
+    dir     => $uikit_path,
     mongodb => true,
-    nginx   => "${boxen}/shared/${darkstar}.conf.erb",
-    nodejs  => "0.12.7",
+    nginx   => "${uikit_file}/shared/$uikit_project.conf.erb",
+    nodejs  => "4.1.1",
     source  => "git@bitbucket.org:torodev/darkstar.git",
   }
 }
